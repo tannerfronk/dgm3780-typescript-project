e3e74f87728c39f4f0a77455a894325c3ae1c635
tannerfronk <tannerjace07@hotmail.com>
2021-02-11 19:57:55 -0700

    Added other interactions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import './styles.css';
 
 let url: string = "http://swapi.dev/api/people/"
+let url2: string = "http://swapi.dev/api/planets/"
 
+let planetArray: Array<string> = []
 
 interface Data {
     url: string
@@ -9,54 +11,62 @@ interface Data {
 type GetCharData = (url: string) => Promise<Data[]>
 
 let charArray: any = []
-const getSwapiData: GetCharData = async (url) => {
+const getCharacterData: GetCharData = async (url) => {
     const res = await fetch(url)
     const data = await res.json()
-    const charData = data.results
+    const charData: JSON = data.results
     return charArray.push(charData)
 }
 
+let planArray: any = []
+const getPlanetData: GetCharData = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    const planData: JSON = data
+    return planArray.push(planData)
+}
+
 window.addEventListener('load', () => {
-getSwapiData(url).then(() =>{ // start of character cards
+getCharacterData(url).then(() =>{ // start of character cards
     for(let i: number = 0; i < charArray[0].length; i++){
         const card = document.createElement('div')
         card.className = "card"
 
-        const title = document.createElement('h2') // title
-        title.innerText = charArray[0][i].name
+        const title: Element = document.createElement('h2') // title
+        title.textContent = charArray[0][i].name
         card.appendChild(title)
 
-        const main = document.querySelector('main') // append to main
+        const main: Element = document.querySelector('main') // append to main
         main.appendChild(card)
 
-        const infoArea = document.createElement("div") // create infoArea
+        const infoArea: Element = document.createElement("div") // create infoArea
         infoArea.className = "infoArea"
         infoArea.className = "hidden"
         infoArea.id = "info"
         card.appendChild(infoArea)
 
-        const charHeight = document.createElement('p') // char height
-        charHeight.innerText = "Height: " + charArray[0][i].height
+        const charHeight: Element = document.createElement('p') // char height
+        charHeight.textContent = "Height: " + charArray[0][i].height
         infoArea.appendChild(charHeight)
 
-        const charMass = document.createElement('p') // char Mass
-        charMass.innerText = "Weight: " + charArray[0][i].mass
+        const charMass: Element = document.createElement('p') // char Mass
+        charMass.textContent = "Weight: " + charArray[0][i].mass
         infoArea.appendChild(charMass)
 
-        const charHair = document.createElement('p') // char hair color
-        charHair.innerText = "Hair Color: " + charArray[0][i].hair_color
+        const charHair: Element = document.createElement('p') // char hair color
+        charHair.textContent = "Hair Color: " + charArray[0][i].hair_color
         infoArea.appendChild(charHair)
         
-        const charBirth = document.createElement('p') // char birth year
-        charBirth.innerText = "Birth Year: " + charArray[0][i].birth_year
+        const charBirth: Element = document.createElement('p') // char birth year
+        charBirth.textContent = "Birth Year: " + charArray[0][i].birth_year
         infoArea.appendChild(charBirth)
 
-        const charEyes = document.createElement('p') // char eye color
-        charEyes.innerText = "Hair Color: " + charArray[0][i].eye_color
+        const charEyes: Element = document.createElement('p') // char eye color
+        charEyes.textContent = "Hair Color: " + charArray[0][i].eye_color
         infoArea.appendChild(charEyes)
 
-        const charGender = document.createElement('p') // char gender
-        charGender.innerText = "Species: " + charArray[0][i].gender
+        const charGender: Element = document.createElement('p') // char gender
+        charGender.textContent = "Species: " + charArray[0][i].gender
         infoArea.appendChild(charGender)
         
 
@@ -67,6 +77,31 @@ getSwapiData(url).then(() =>{ // start of character cards
     }
 })
 
+})
 
+getPlanetData(url2).then(() =>{
+    for(let i: number = 0; i < planArray[0].results.length; i++){
+        planetArray.push(planArray[0].results[i].name)
+    }
+    const planetArea: Element = document.querySelector('#planets') // append to main
+    const planets: Element = document.createElement('p')
+    planets.textContent = planetArray.join(", ").toString()
+    planetArea.appendChild(planets)
+    return planetArray
+})
 
+document.getElementById('sort').addEventListener("click", function() { // sort button
+    let newString: Element = document.querySelector('#planets')
+    newString.textContent = planetArray.sort().join(", ")
+})
+
+document.getElementById('pop').addEventListener("click", function() { // delete planet button
+    let newString: Element = document.querySelector('#planets')
+    planetArray.pop()
+    newString.textContent = planetArray.join(", ")
+})
+
+document.getElementById('reverse').addEventListener("click", function() { // add planets button
+    let newString: Element = document.querySelector('#planets')
+    newString.textContent = planetArray.reverse().join(", ")
 })

d40917d9fe21f36b87099022b1f34a1a0bfd07be
tannerfronk <tannerjace07@hotmail.com>
2021-02-11 17:51:49 -0700

    finished character info cards

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const getCharacterData: GetCharData = async (url) => {
 
 window.addEventListener('load', () => {
 getCharacterData(url).then(() =>{
-    console.log(charArray[0][1])
+    console.log(charArray[0][1].species)
     for(let i: number = 0; i < charArray[0].length; i++){
         const card = document.createElement('div')
         card.className = "card"
@@ -40,6 +40,25 @@ getCharacterData(url).then(() =>{
         charHeight.innerText = "Height: " + charArray[0][i].height
         infoArea.appendChild(charHeight)
 
+        const charMass = document.createElement('p') // char Mass
+        charMass.innerText = "Weight: " + charArray[0][i].mass
+        infoArea.appendChild(charMass)
+
+        const charHair = document.createElement('p') // char hair color
+        charHair.innerText = "Hair Color: " + charArray[0][i].hair_color
+        infoArea.appendChild(charHair)
+        
+        const charBirth = document.createElement('p') // char birth year
+        charBirth.innerText = "Birth Year: " + charArray[0][i].birth_year
+        infoArea.appendChild(charBirth)
+
+        const charEyes = document.createElement('p') // char eye color
+        charEyes.innerText = "Hair Color: " + charArray[0][i].eye_color
+        infoArea.appendChild(charEyes)
+
+        const charGender = document.createElement('p') // char gender
+        charGender.innerText = "Species: " + charArray[0][i].gender
+        infoArea.appendChild(charGender)
         
 
         card.addEventListener("click", function() { // info toggle
